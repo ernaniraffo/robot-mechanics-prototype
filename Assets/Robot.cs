83de85cc6f9ebfc9cb31ab08987fbdc1b3118f45
Ernani Raffo <ernani.raffo@icloud.com>
2024-11-28 16:03:04 -0800

    player still abruptly stopping with no input :(

--- Assets/Robot.cs
+++ Assets/Robot.cs
@@ -9,8 +9,14 @@ public class Robot : MonoBehaviour
     private Vector3 playerVelocity;
     private bool groundedPlayer;
     private float gravityValue = -9.81f;
-    private float playerSpeed = 2.0f;
+    private float playerSpeed = 5.0f;
     private float jumpHeight = 1.0f;
+    private float rotationSpeed = 25f;
+
+    Vector2 input;
+    bool jumpInput;
+    private float angle;
+    Quaternion targetRotation;
 
     // Start is called before the first frame update
     void Start()
@@ -21,35 +27,87 @@ public class Robot : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // min move distance set to 0 to avoid inconsistency in grounded player
-        groundedPlayer = controller.isGrounded;
+        GetInput();
+
+        // Jump if needed and fall
+        if (jumpInput && IsGrounded()) {
+            Jump();
+        }
+        Fall();
+        
+        // if no input, do not rotate !
+        if (Mathf.Abs(input.x) < 1 && Mathf.Abs(input.y) < 1) {
+            Debug.Log("Stop rotating!");
+            return;
+        }
+        // Debug.Log("Movement: " + input);
+
+        CalculateDirection();
+        Rotate();
+        Move();
+    }
+
+    /// <summary>
+    /// Get the raw input from player. Value for axis is either -1, 0, 1.
+    /// </summary>
+    void GetInput() {
+        input.x = Input.GetAxisRaw("Horizontal");
+        input.y = Input.GetAxisRaw("Vertical");
+        jumpInput = Input.GetButton("Jump");
+    }
+
+    /// <summary>
+    /// Perform a jump.
+    /// </summary>
+    void Jump() {
+        Debug.Log("Performing a jump");
+        playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
+    }
+
+    /// <summary>
+    /// Move the player towards input direction.
+    /// </summary>
+    void Move() {
+        Vector3 movementDirection = new Vector3(input.x, 0, input.y);
+        controller.Move(movementDirection * playerSpeed * Time.deltaTime);
+    }
+
+    /// <summary>
+    /// Check if the player is grounded.
+    /// </summary>
+    /// <returns>bool</returns>
+    bool IsGrounded() {
+        return controller.isGrounded;
+    }
+
+    /// <summary>
+    /// Make the player fall downwards according to gravity. Set player velocity to 0. 
+    /// </summary>
+    void Fall() {
+        groundedPlayer = IsGrounded();
+        // clamp player velocity
         if (groundedPlayer && playerVelocity.y < 0) {
             playerVelocity.y = 0f;
         }
-
-        // move the player based on input direction
-        Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        controller.Move(move * Time.deltaTime * playerSpeed);
-
-        // set the forward vector of the gameobject to the direction we are going
-        // TODO: fix robot's forward resetting abruptly
-        if (move != Vector3.zero) {
-            gameObject.transform.forward = move;
-        }
-
-        // make the player jump based on input
-        if (Input.GetButton("Jump") && groundedPlayer) {
-            Debug.Log("Performing a jump");
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -2.0f * gravityValue);
-        }
-
-        // move the player according to gravity value
+        // make the player fall according to gravity
         playerVelocity.y += gravityValue * Time.deltaTime;
-        
-        // move the player based on velocity
         controller.Move(playerVelocity * Time.deltaTime);
+    }
 
-        // draw ray to show player's forward direction
-        Debug.DrawRay(gameObject.transform.position, gameObject.transform.forward * 5, Color.red);
+    /// <summary>
+    /// Direction relative to the camera's rotation
+    /// </summary>
+    void CalculateDirection() {
+        angle = Mathf.Atan2(input.x, input.y);
+        angle = Mathf.Rad2Deg * angle;
+        // Debug.Log("Angle: " + angle);
+    }
+
+    /// <summary>
+    /// Rotate towards the calculate angle
+    /// </summary>
+    void Rotate() {
+        targetRotation = Quaternion.Euler(0, angle, 0);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
     }
 }
